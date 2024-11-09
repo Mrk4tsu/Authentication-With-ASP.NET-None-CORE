bd396cc9ab69a50e63c1b3d2281ba109967bcf62
MrKatsu <mrk4tsu@gmail.com>
2024-11-09 14:28:07 +0700

    Thêm chức năng tạo tài khoản

---
+++ Authentication/Services/BaseDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Authentication.Services
+{
+    public abstract class BaseDAO
+    {
+        protected string Lower(string value)
+        {
+            return value = value.ToLowerInvariant();
+        }       
+        protected string GenerateUserCODE()
+        {
+            long ticks = DateTime.Now.Ticks;
+            string result = $"{RandomString(2)}{ticks}";
+            return result;
+        }
+        private string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ Authentication/Services/UserDAO.cs
@@ -0,0 +1,63 @@
+﻿using Authentication.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentication.Services
+{
+    public class UserDAO : BaseDAO
+    {
+        private AuthsDbContext db;
+        public UserDAO()
+        {
+            db = new AuthsDbContext();
+        }
+        #region[Get User]
+        public Users GetUserReadOnly(string username)
+        {
+            var model = db.Users.AsNoTracking().SingleOrDefault(u => Lower(u.Username) == Lower(username));
+            return model;
+        }
+        public async Task<Users> GetUsersAsync(int id)
+        {
+            var model = await db.Users.SingleOrDefaultAsync(u => u.Id == id);
+            return model;
+        }
+        #endregion
+        public async Task<int> InsertUserAsync(Users model)
+        {
+            if (await IsExistingAccount(model.Username))
+            {
+                return -1;
+            }
+            if (await IsExistingEmail(model.Email))
+            {
+                return -2;
+            }
+            model.UserCODE = GenerateUserCODE();
+            model.CreatedDate = DateTime.UtcNow;
+            model.ModifiedDate = DateTime.UtcNow;
+            model.Status = true;
+            db.Users.Add(model);
+            await db.SaveChangesAsync().ConfigureAwait(false);
+            return model.Id;
+        }
+        public async Task<bool> IsExistingAccount(string username)
+        {
+            return await db.Users
+                .AsNoTracking()
+                .Where(a => a.Username == username)
+                .Select(a => a.Id)
+                .AnyAsync();
+        }
+        public async Task<bool> IsExistingEmail(string mail)
+        {
+            return await db.Users
+                .AsNoTracking()
+                .Where(a => a.Email == mail)
+                //.Select(a => a.Id)
+                .AnyAsync();
+        }
+    }
+}
